aa30455ccbcca449b4511a38c82f3e260e69143e
Kevin Martin <kevin@balance.ca>
2026-02-19 18:02:11 -0500

    Adds support for parsing auxiliary metadata.

--- auxiliary_data.go
+++ auxiliary_data.go
@@ -38,25 +38,45 @@ func (d *AuxiliaryData) MarshalCBOR() ([]byte, error) {
 func (d *AuxiliaryData) UnmarshalCBOR(data []byte) error {
 	type auxiliaryData AuxiliaryData
 
-	// Register tag 259 for maps
-	tags, err := d.tagSet(auxiliaryData{})
-	if err != nil {
-		return err
+	// Alonzo+ auxiliary data is CBOR tag 259. Legacy metadata is an untagged map.
+	// Support both forms to decode externally generated transactions.
+	if hasCBORTag(stripSelfDescribedCBORTag(data)) {
+		tags, err := d.tagSet(auxiliaryData{})
+		if err != nil {
+			return err
+		}
+
+		dm, err := cbor.DecOptions{
+			MapKeyByteString: cbor.MapKeyByteStringWrap,
+		}.DecModeWithSharedTags(tags)
+		if err != nil {
+			return err
+		}
+
+		var dd auxiliaryData
+		if err := dm.Unmarshal(data, &dd); err != nil {
+			return err
+		}
+
+		*d = AuxiliaryData(dd)
+		return nil
 	}
 
 	dm, err := cbor.DecOptions{
 		MapKeyByteString: cbor.MapKeyByteStringWrap,
-	}.DecModeWithTags(tags)
+	}.DecMode()
 	if err != nil {
 		return err
 	}
 
-	var dd auxiliaryData
-	if err := dm.Unmarshal(data, &dd); err != nil {
+	var md Metadata
+	if err := dm.Unmarshal(data, &md); err != nil {
 		return err
 	}
-	d.Metadata = dd.Metadata
 
+	d.Metadata = md
+	d.NativeScripts = nil
+	d.PlutusScripts = nil
 	return nil
 }
 
@@ -71,4 +91,15 @@ func (d *AuxiliaryData) tagSet(contentType interface{}) (cbor.TagSet, error) {
 	}
 
 	return tags, nil
+}
+
+func stripSelfDescribedCBORTag(data []byte) []byte {
+	for len(data) >= 3 && data[0] == 0xd9 && data[1] == 0xd9 && data[2] == 0xf7 {
+		data = data[3:]
+	}
+	return data
+}
+
+func hasCBORTag(data []byte) bool {
+	return len(data) > 0 && (data[0]&0xe0) == 0xc0
 }
